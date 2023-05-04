ec6c2792d315b24e47f072d6358518cbfb653f2b
Oskar Kosobucki <43652764+Kosaaaaa@users.noreply.github.com>
2023-05-04 17:09:18 +0200

    ignore pylint errors

--- blog_app/utils/storages.py
+++ blog_app/utils/storages.py
@@ -1,11 +1,11 @@
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage  # pylint: disable=import-error
 
 
-class StaticRootS3Boto3Storage(S3Boto3Storage):
+class StaticRootS3Boto3Storage(S3Boto3Storage):  # pylint: disable=too-few-public-methods
     location = "static"
     default_acl = "public-read"
 
 
-class MediaRootS3Boto3Storage(S3Boto3Storage):
+class MediaRootS3Boto3Storage(S3Boto3Storage):  # pylint: disable=too-few-public-methods
     location = "media"
     file_overwrite = False
